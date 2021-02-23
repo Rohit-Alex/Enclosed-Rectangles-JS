3eba01b1a0ac4c2f4e1d70261ecb41dec6a8a8ac
Rohit Kumar <75325602+Rohit-Alex@users.noreply.github.com>
2021-02-23 09:33:18 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,69 +6,73 @@
 //      children: []
 //}
 
-function updateStructure(rec1,rec2){
-	var top1 = rec1.top, top2 = rec2.top;
-	var left1 = rec1.left, left2 = rec2.left;
-	var width1 = rec1.width, width2 = rec2.width;
-    var height1 = rec1.height, height2 = rec2.height;
+function updateStructure(rec1, rec2) {
+  //write your code
+  if (contains(rec1, rec2)) {
+    const relDimension = relativeDim(rec1, rec2);
+    return { ...rec1, children: [relDimension] };
+  } else if (contains(rec2, rec1)) {
+    const relDimension = relativeDim(rec2, rec1);
+    return { ...rec2, children: [relDimension] };
+  } else {
+    return { ...rec1 };
+  }
+}
+function contains(recA, recB) {
+  const rec1 = normalise(recA);
+  const rec2 = normalise(recB);
+  if (
+    rec1.x1 <= rec2.x1 &&
+    rec1.x2 <= rec2.x2 &&
+    rec1.x3 >= rec2.x3 &&
+    rec1.x4 >= rec2.x4
+  )
+    return true;
+  return false;
+}
+function relativeDim(recA, recB) {
+  const rec1 = normalise(recA);
+  const rec2 = normalise(recB);
+  const rec = {
+    children: recB.children
+  };
 
-        top1 = top1.slice(0,-1); top1 = top1.slice(0,-1);
-        top1 = Number(top1);
-        top2 = top2.slice(0,-1); top2 = top2.slice(0,-1);
-        top2 = Number(top2);
-        
-        left1 = left1.slice(0,-1); left1 = left1.slice(0,-1);
-        left1 = Number(left1);
-        left2 = left2.slice(0,-1); left2 = left2.slice(0,-1);
-        left2 = Number(left2);
-        
-        width1 = width1.slice(0,-1); width1 = width1.slice(0,-1);
-        width1 = Number(width1);
-        width2 = width2.slice(0,-1); width2 = width2.slice(0,-1);
-        width2 = Number(width2);
-        
-        height1 = height1.slice(0,-1); height1 = height1.slice(0,-1);
-        height1 = Number(height1);
-        height2 = height2.slice(0,-1); height2 = height2.slice(0,-1);
-        height2 = Number(height2);
-        
-        if(top1 > top2 && left1 > left2 && height2+top2 > height1+top1 && width2+left2 > width1+left1){
-            return {
-                top: rec2.top,
-                left: rec2.left,
-                width: rec2.width,
-                height: rec2.height,
-                    children: [{
-                    top: `${top1-top2}px`,
-                    left: `${left1-left2}px`,
-                    width: `${width2-width1}px`,
-                    height: `${height2-height1}px`,
-                    children: []
-                }]
-            }
-        }else if(top1 <= top2 && left1 <= left2 && (top2+height2) <= (top1+height1) && (left2 + width2) <= (left1 + width1)){
-            return {
-                top: rec1.top,
-                left: rec1.left,
-                width: rec1.width,
-                height: rec1.height,
-                    children: [{
-                    top: `${top2-top1}px`,
-                    left: `${left2-left1}px`,
-                    width: `${width1-width2}px`,
-                    height: `${height1-height2}px`,
-                    children: []
-                }]
-            }
-        }else {
-            return {
-                top: rec1.top,
-                left: rec1.left,
-                width: rec1.width,
-                height: rec1.height,
-                children: []
-            }
-        }
+  if (recB.top) {
+    rec.top = `${rec2.x1 - rec1.x1}px`;
+  }
+  if (recB.left) {
+    rec.left = `${rec2.x2 - rec1.x2}px`;
+  }
+  if (recB.height) {
+    rec.height = recB.height;
+  }
+  if (recB.width) {
+    rec.width = recB.width;
+  }
+  if (recB.bottom) {
+    rec.bottom = `${rec1.x3 - rec2.x3}px`;
+  }
+  if (recB.right) {
+    rec.right = `${rec1.x4 - rec2.x4}px`;
+  }
+  return rec;
+}
+function normalise(rec) {
+  return {
+    x1: rec.top
+      ? parseInt(rec.top)
+      : -(parseInt(rec.height) + parseInt(rec.bottom)),
+    x2: rec.left
+      ? parseInt(rec.left)
+      : -(parseInt(rec.width) + parseInt(rec.right)),
+    x3: rec.bottom
+      ? -parseInt(rec.bottom)
+      : parseInt(rec.height) + parseInt(rec.top),
+    x4: rec.right
+      ? -parseInt(rec.right)
+      : parseInt(rec.width) + parseInt(rec.left),
+  };
 }
 
 module.exports = updateStructure;
+
